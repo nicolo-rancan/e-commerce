751d8c247eff54fc7b4d90108e0b5c95f7024e2e
matteo.bauce.vi@gmail.com <matteo.bauce.vi@gmail.com>
2024-11-11 22:59:25 +0100

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,52 @@
+import Image from "next/image";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer>
       <div className="wrapper">
-        <p>test</p>
+        <div className="social">
+          <Image src="/next.svg" width={214} height={15} alt="" />
+          <p>
+            Li Europan lingues es membres del sam familie. Lor separat existentie es un myth.
+            Por scientie, musica, sport etc, litot Europa usa li sam vocabular.
+          </p>
+          <div className="links">
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram />
+            </a>
+
+            <a
+              href="https://www.facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook />
+            </a>
+
+            <a
+              href="https://www.twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
+          </div>
+        </div>
+        <div className="information">
+          <h2>Informazioni</h2>
+        </div>
+        <div className="privacy">
+          <h2>Termini & Condizioni</h2>
+        </div>
+        <div className="feedback">
+          <h2>FeedBack</h2>
+        </div>
       </div>
     </footer>
   );
