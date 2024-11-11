94e924bb05917cd9e0d9aa9902b0290dafdda174
Nicolò Rancan <nicolo.rancan.gm@gmail.com>
2024-11-11 21:50:04 +0100

    change

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -24,8 +24,18 @@ const Home = () => {
             <Button variant={"ghost"}>Come acquistare?</Button>
           </div>
           <div className={styles.protip}>
-            <img src="" alt="" />
+            <img src="/test.png" alt="" />
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem sequi reprehenderit unde suscipit autem tempore commodi asperiores maxime perferendis quas?</p>
+          </div>
+        </div>
+      </section>
+      <section className={styles.section}>
+        <div className={styles.centerCard}>
+          <div className={styles.left}>
+            <h2>Lorem ipsum dolor sit amet consectetur.</h2>
+          </div>
+          <div className={styles.right}>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis veritatis animi amet aliquam, ab sit modi voluptatibus facilis dolor dicta!</p>
           </div>
         </div>
       </section>
